e08d23ae0603f7b51079d2c7cdb37d3d52a4dbd0
Chotato <77442976+Chotato@users.noreply.github.com>
2021-02-16 10:46:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,87 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground, box1B, box2B, box3B;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+    box1 = createSprite(310, 610, 20, 100);
+	box2 = createSprite(width/2, 650, 200, 20);
+    box3 = createSprite(510, 610, 20, 100);
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, packageBody);
+
+
+    box1B = Bodies.rectangle(310, 590, 20, 100, {isStatic:true});
+	World.add(world, box1B);
+
+    box2B = Bodies.rectangle(width/2, 630, 200, 20, {isStatic:true});
+	World.add(world, box2B);
+
+    box3B = Bodies.rectangle(510, 590, 20, 100, {isStatic:true});
+	World.add(world, box3B);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+
+  Engine.update(engine);
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  if(keyDown("down")){
+	Matter.Body.setStatic(packageBody, false);
+
+  }
+  box1.shapeColor = "red";
+  box2.shapeColor = "red";
+  box3.shapeColor = "red";
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.    
+  }
+}
+
+
+
